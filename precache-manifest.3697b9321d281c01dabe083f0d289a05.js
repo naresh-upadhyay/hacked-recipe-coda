199c65a6b74db6319cb8b86299d7834f3d1f6012
naresh-upadhyay <naresh.king88898@gmail.com>
2020-08-10 00:49:35 +0530

    Updates

---
+++ precache-manifest.3697b9321d281c01dabe083f0d289a05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cd639191a115f642c16a346f362fd00",
+    "url": "/hacked-recipe-coda/index.html"
+  },
+  {
+    "revision": "9da14c9c4fb92932a1a5",
+    "url": "/hacked-recipe-coda/static/css/2.bff19397.chunk.css"
+  },
+  {
+    "revision": "5097b09e0386a3ca97a9",
+    "url": "/hacked-recipe-coda/static/css/main.0564d7cd.chunk.css"
+  },
+  {
+    "revision": "9da14c9c4fb92932a1a5",
+    "url": "/hacked-recipe-coda/static/js/2.8602339b.chunk.js"
+  },
+  {
+    "revision": "5097b09e0386a3ca97a9",
+    "url": "/hacked-recipe-coda/static/js/main.91bdf21c.chunk.js"
+  },
+  {
+    "revision": "c41004d72e3edfa57857",
+    "url": "/hacked-recipe-coda/static/js/runtime~main.b5bdd8f3.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/hacked-recipe-coda/static/media/SEGA.12a3db8b.woff"
+  }
+]);
